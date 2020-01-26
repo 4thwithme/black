73b5a24a621fc75e22b0c0c6fd694aed70cb9d6b
4thwithme <aopopenko@gmail.com>
2020-01-26 15:41:00 +0200

    try 1

--- src/clientConfig/socketConfig.ts
+++ src/clientConfig/socketConfig.ts
@@ -4,8 +4,8 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   socketHost = "ws://localhost:8888";
-} else if (hostname === "blackq.herokuapp.com.") {
-  socketHost = "ws://blackq.herokuapp.com.:8888";
+} else if (hostname === "blackq.herokuapp.com") {
+  socketHost = "ws://blackq.herokuapp.com:8888";
 } else {
   socketHost = "ws://127.0.0.1:8888";
 }
