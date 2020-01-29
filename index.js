486349847bdb81148c7c175516bf4a092c8c9e6a
4thwithme <aopopenko@gmail.com>
2020-01-29 19:26:57 +0200

    try

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ app.use(cookieParser());
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(express.json({ limit: "50mb" }));
 
+app.use(express.static(path.resolve(__dirname, "build")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
@@ -39,8 +41,6 @@ app.get("*", (req, res) => {
 app.use("/api/users", usersRoute);
 app.use("/api/auth", authRouter);
 app.use("/api/chat", chatRouter);
-
-app.use(express.static(path.resolve(__dirname, "build")));
 
 mongoose
   .connect(
